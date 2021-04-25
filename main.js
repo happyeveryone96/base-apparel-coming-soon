f0acb525389019deca345d82b8c683ea2fc51fae
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2021-04-25 21:48:04 +0900

    Update main.js

--- main.js
+++ main.js
@@ -4,18 +4,18 @@ const emailBtn = document.getElementById("email-button");
 emailBtn.addEventListener('click', function (event) {
     const email = document.getElementById('email');
     const emailVal = email.value;
-    const alertEmail = document.getElementById('alert-email');
     const regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+    const error = document.getElementById('error');
     event.preventDefault();
     if (emailVal == "") {
-        // document.getElementById('alert-email').innerHTML = "이메일을 입력하세요";
-        email.placeholder = "이메일을 입력하세요";
+        document.getElementById('alert-email').innerHTML = "이메일을 입력하세요";
+        error.style.display = "block";
     } 
     else if (emailVal.match(regExp) != null) {
         document.getElementById('alert-email').innerHTML = "입력 완료";
+        error.style.display = "none";
     } else {
         document.getElementById('alert-email').innerHTML = "이메일 형식에 맞지 않습니다.";
-        alertEmail.style.left = '350px';
-
+        error.style.display = "block";
     }
 });
